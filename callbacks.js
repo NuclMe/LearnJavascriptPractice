4478557279f405ac94782c7857ec4e5cbf8c5ce9
NuclMe <saltansergei@gmail.com>
2021-10-04 19:12:08 +0300

    started codwars

--- callbacks.js
+++ callbacks.js
@@ -1,10 +1,23 @@
-function loadScript(src, callback) {
-  let script = document.createElement('script')
-  script.src = src
-  script.onload = () => callback(script)
+// function loadScript(src, callback) {
+//   debugger
+//   let script = document.createElement('script')
+//   script.src = src
+//   script.onload = () => callback(script)
 
-  document.head.append(script)
+//   document.head.append(script)
+// }
+
+// loadScript('./index.js', (scipt = () => {}))
+// alert('iii') // вызовется первым
+
+let name = 'alex'
+function hello(name) {
+  if (name === ' ') {
+    return 'Hello world'
+  } else {
+    name.charAt(0)
+    return `Hello, ${name}`
+  }
 }
 
-loadScript('./index.js')
-alert('iii') // вызовется первым
+hello()
