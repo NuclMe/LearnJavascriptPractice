3b910784d41d6e647a08f58f14d3695aa5c1f872
NuclMe <saltansergei@gmail.com>
2021-10-24 00:26:42 +0300

    added one seven-lvl cata

--- catas.js
+++ catas.js
@@ -4,7 +4,7 @@ function changeName(names) {
   for (let i = names.length - 1; i > 0; i--) {
     newReverse += names[i]
   }
-  console.log(newReverse)
+  // console.log(newReverse)
   return newReverse
 }
 
@@ -18,14 +18,21 @@ function wordsReverse(names) {
   let res = []
   for (var i = 0; i < words.length; i++) {
     // res.push(words[i].split('').reverse().join(''))
-    console.log(words[i].split('').reverse().join(''))
+    // console.log(words[i].split('').reverse().join(''))
   }
   return res.join(' ')
 }
 
 function wordReverse(names) {
   let makeArr = names.split('')
-  console.log(typeof makeArr)
+  // console.log(typeof makeArr)
 }
 
 wordsReverse(names)
+let str = 'mama'
+const textToBinary = (str = '') => {
+  let res = ''
+  res = str.split('').map((item) => {
+    return char.charCodeAt(0).toString(2)
+  })
+}
